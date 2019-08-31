3e1beb1d99a58ae631c612d60c3b0622fcf7d868
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-08-31 22:53:01 +0900

    CollectionViewCellのImageViewとLabelをCollectionViewCellクラスに関連づける

--- HorizontalCollectionView/HorizontalCollectionView/CollectionViewCell.swift
+++ HorizontalCollectionView/HorizontalCollectionView/CollectionViewCell.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
