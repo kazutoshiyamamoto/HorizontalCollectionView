2db7a08ca5032d65ba71225a41c198a6996fb1cf
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-08-31 22:59:31 +0900

    TableViewCellにxibファイルを登録する

--- HorizontalCollectionView/HorizontalCollectionView/TableViewCell.swift
+++ HorizontalCollectionView/HorizontalCollectionView/TableViewCell.swift
@@ -9,18 +9,16 @@
 import UIKit
 
 class TableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var collectionView: UICollectionView!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: "CollectionViewCell")
     }
     
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
 }
