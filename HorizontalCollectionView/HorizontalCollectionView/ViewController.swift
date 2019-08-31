457b75d73db445ced77c7460cf8c4efe8526c7ba
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-08-31 23:31:59 +0900

    TableViewのDelegateメソッドを追加

--- HorizontalCollectionView/HorizontalCollectionView/ViewController.swift
+++ HorizontalCollectionView/HorizontalCollectionView/ViewController.swift
@@ -27,6 +27,10 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        
 //        self.collectionView.register(UINib(nibName: "CollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "CollectionViewCell")
 //
 //        self.collectionView.delegate = self
@@ -36,7 +40,7 @@ class ViewController: UIViewController {
 //        self.setUpPixabayItems()
 
         let refreshControl = UIRefreshControl()
-        self.collectionView.refreshControl = refreshControl
+        self.tableView.refreshControl = refreshControl
         refreshControl.addTarget(self, action: #selector(ViewController.refresh(sender:)), for: .valueChanged)
     }
 
@@ -49,7 +53,7 @@ class ViewController: UIViewController {
         self.getPixabayItems(completion: { (item) in
             self.items = item
             DispatchQueue.main.async {
-                self.collectionView.reloadData()
+                self.tableView.reloadData()
             }
         })
     }
@@ -75,56 +79,73 @@ class ViewController: UIViewController {
     }
 }
 
-// セル選択時の処理
-extension ViewController: UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        if let url = URL(string: self.items?.hits[indexPath.row].webformatURL ?? "") {
-            UIApplication.shared.open(url)
-        }
-    }
-}
-
-// セルの大きさ
-extension ViewController:  UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-
-        let numberOfCell: CGFloat = 3
-        let cellWidth = UIScreen.main.bounds.size.width / numberOfCell - 2
-        return CGSize(width: cellWidth, height: cellWidth)
-    }
-}
-
-extension ViewController: UICollectionViewDataSource {
-    // セルの数
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.items?.hits.count ?? 0
-    }
-
-    // セルの設定
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
-
-        cell.imageView.image = nil
-        cell.label.text = nil
-
-        let item = self.items?.hits[indexPath.row]
-        cell.label.text = item?.user
-        let previewUrl = URL(string: item?.previewURL ?? "")!
-        Nuke.loadImage(with: previewUrl, into: cell.imageView)
-
+extension ViewController: UITableViewDelegate {
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        
         return cell
     }
 }
 
-extension ViewController: UICollectionViewDataSourcePrefetching {
-    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
-        let urls = indexPaths.map { URL(string: self.items?.hits[$0.row].previewURL ?? "") }
-        preheater.startPreheating(with: urls as! [URL])
-    }
-
-    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
-        let urls = indexPaths.map { URL(string: self.items?.hits[$0.row].previewURL ?? "") }
-        preheater.stopPreheating(with: urls as! [URL])
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        guard let cell = cell as? TableViewCell else { return }
+        
+//        //ShopTableViewCell.swiftで設定したメソッドを呼び出す
+//        cell.setCollectionViewDataSourceDelegate(dataSourceDelegate: self, forRow: indexPath.row)
     }
 }
+
+//// セル選択時の処理
+//extension ViewController: UICollectionViewDelegate {
+//    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        if let url = URL(string: self.items?.hits[indexPath.row].webformatURL ?? "") {
+//            UIApplication.shared.open(url)
+//        }
+//    }
+//}
+//
+//// セルの大きさ
+//extension ViewController:  UICollectionViewDelegateFlowLayout {
+//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//        let numberOfCell: CGFloat = 3
+//        let cellWidth = UIScreen.main.bounds.size.width / numberOfCell - 2
+//        return CGSize(width: cellWidth, height: cellWidth)
+//    }
+//}
+//
+//extension ViewController: UICollectionViewDataSource {
+//    // セルの数
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return self.items?.hits.count ?? 0
+//    }
+//
+//    // セルの設定
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+//
+//        cell.imageView.image = nil
+//        cell.label.text = nil
+//
+//        let item = self.items?.hits[indexPath.row]
+//        cell.label.text = item?.user
+//        let previewUrl = URL(string: item?.previewURL ?? "")!
+//        Nuke.loadImage(with: previewUrl, into: cell.imageView)
+//
+//        return cell
+//    }
+//}
+//
+//extension ViewController: UICollectionViewDataSourcePrefetching {
+//    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+//        let urls = indexPaths.map { URL(string: self.items?.hits[$0.row].previewURL ?? "") }
+//        preheater.startPreheating(with: urls as! [URL])
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cancelPrefetchingForItemsAt indexPaths: [IndexPath]) {
+//        let urls = indexPaths.map { URL(string: self.items?.hits[$0.row].previewURL ?? "") }
+//        preheater.stopPreheating(with: urls as! [URL])
+//    }
+//}
 
