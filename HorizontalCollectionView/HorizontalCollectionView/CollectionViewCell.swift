e4130fa489be913154737d0f9a94666774b60b81
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-08-31 22:51:03 +0900

    CollectionViewCellの追加

---
+++ HorizontalCollectionView/HorizontalCollectionView/CollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CollectionViewCell.swift
+//  HorizontalCollectionView
+//
+//  Created by home on 2019/08/31.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ HorizontalCollectionView/Pods/Target Support Files/Pods-HorizontalCollectionView/Pods-HorizontalCollectionView-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_HorizontalCollectionView : NSObject
+@end
+@implementation PodsDummy_Pods_HorizontalCollectionView
+@end
