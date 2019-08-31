2722012576d293ded377dde498678e64378bac27
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-08-31 22:51:20 +0900

    TableViewCellの追加

---
+++ HorizontalCollectionView/HorizontalCollectionView/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  HorizontalCollectionView
+//
+//  Created by home on 2019/08/31.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
